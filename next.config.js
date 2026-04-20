8974d760c65766b306924ae15ccfaaac2df82b26
celinaapu <apucelina13@gmail.com>
2026-04-20 18:52:16 +0100

    fix: add netlify next.js plugin, remove bad api redirect

--- next.config.js
+++ next.config.js
@@ -1,14 +1,38 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     serverActions: true,
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'res.cloudinary.com',
+//         pathname: `/${process.env.CLOUDINARY_CLOUD_NAME}/**`,
+//       },
+//     ],
+//   },
+//   webpack: (config) => {
+//     config.externals.push({
+//       "utf-8-validate": "commonjs utf-8-validate",
+//       bufferutil: "commonjs bufferutil",
+//     });
+//     return config;
+//   },
+// };
+
+// module.exports = nextConfig;
+
+
+cat > next.config.js << 'EOF'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
-        pathname: `/${process.env.CLOUDINARY_CLOUD_NAME}/**`,
+        pathname: '/**',
       },
     ],
   },
@@ -20,5 +44,5 @@ const nextConfig = {
     return config;
   },
 };
-
 module.exports = nextConfig;
+EOF
